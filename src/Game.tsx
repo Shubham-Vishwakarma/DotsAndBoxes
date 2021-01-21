91672ab8ca80fbb58539c6e7c82829f174e44c2e
Shubham-Vishwakarma <shubhamvishwakarma71@gmail.com>
2021-01-21 23:27:36 +0530

    Inline CSS using React Props to set values

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,28 @@
 // import css
 import './index.css'
 
+// import React from 'react';
+import CSS from 'csstype';
+
 import GameBoard from './GameBoard'
 
-function Game(){
+const gameWidth: number = Math.floor(window.innerWidth * 0.6);
+const gameHeight: number = window.innerHeight;
+
+const cssStyle: CSS.Properties = {
+    height: String(gameHeight).concat('px'),
+    width: String(gameWidth).concat('px'),
+    backgroundColor: 'red',
+    position: 'relative',
+    margin: 'auto'
+}
+
+console.log(cssStyle);
+
+const Game = () => {
     return (
-        <div className={'gameSection'}>
-            <GameBoard />
+        <div className='gameSection' style={cssStyle}>
+            <GameBoard parentWidth={gameWidth} parentHeight={gameHeight} />
         </div>
     );
 }
--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,10 +1,47 @@
 import './index.css'
 
-function GameBoard(){
-    return(
-        <div className={'gameBoard'} id={'gameBoard'}>
-        </div>
-    );
+import React from 'react';
+import CSS from 'csstype';
+
+
+type Props = {
+    parentWidth: number,
+    parentHeight: number
+}
+
+type State = {
+
+}
+
+const cssStyle: CSS.Properties = {
+    backgroundColor: 'yellow',
+    position: 'relative',
+    margin: 'auto'
+}
+
+function min(a: number, b: number): number {
+    return a < b ? a : b;
+}
+
+class GameBoard extends React.Component<Props, State>{
+
+    render() {
+
+        let size = min(this.props.parentHeight, this.props.parentWidth);
+        size = Math.floor(size*0.8);
+
+        let top = (this.props.parentHeight - size)/2;
+
+        cssStyle.height = String(size).concat('px');
+        cssStyle.width = String(size).concat('px');
+        cssStyle.top = String(top).concat('px');
+
+        return(
+            <div className='gameBoard' style={cssStyle}>
+            </div>
+        );
+    }
+
 }
 
 export default GameBoard;
