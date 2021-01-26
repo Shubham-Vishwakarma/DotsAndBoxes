b3a8540e087f054881fc4ebd0e9e3e6c56e6d0c9
Shubham-Vishwakarma <shubhamvishwakarma71@gmail.com>
2021-01-26 14:37:42 +0530

    Resize Window

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,26 +1,61 @@
 // import css
 import './index.css'
 
-// import React from 'react';
+
 import CSS from 'csstype';
 
 import GameBoard from './GameBoard'
+import { useEffect, useState } from 'react';
 
-const gameWidth: number = Math.floor(window.innerWidth * 0.6);
-const gameHeight: number = window.innerHeight;
-
-const cssStyle: CSS.Properties = {
-    height: String(gameHeight).concat('px'),
-    width: String(gameWidth).concat('px'),
-    backgroundColor: 'red',
-    position: 'relative',
-    margin: 'auto'
+interface Dimension{
+    height: number,
+    width: number
 }
 
-const Game = () => {
+function Game(){
+
+    const [windowDimensions, setWindowDimensions] = useState<Dimension>({height: window.innerHeight, width: window.innerWidth});
+    const [gameBoardDimensions, setGameBoardDimensions] = useState<Dimension>({height: window.innerHeight, width: Math.floor(windowDimensions.width * 0.6)});
+    const [gameBoardCssStyle, setGameBoardCssStyle] = useState<CSS.Properties>({
+                                                            height: String(gameBoardDimensions.height).concat('px'),
+                                                            width: String(gameBoardDimensions.width).concat('px'),
+                                                            backgroundColor: 'red',
+                                                            position: 'relative',
+                                                            margin: 'auto'
+                                                        });
+
+    useEffect(() => {
+        const resizeWindow = () => {
+            setWindowDimensions({
+                height: window.innerHeight,
+                width: window.innerWidth
+            });
+        };
+
+        window.addEventListener('resize', resizeWindow);
+
+        return () => (window.removeEventListener('resize', resizeWindow));
+    }, [windowDimensions]);
+
+    useEffect(() => {
+        setGameBoardDimensions({
+            height: Math.floor(windowDimensions.height),
+            width: Math.floor(windowDimensions.width * 0.6)
+        });
+
+        setGameBoardCssStyle({
+            height: String(gameBoardDimensions.height).concat('px'),
+            width: String(gameBoardDimensions.width).concat('px'),
+            backgroundColor: 'red',
+            position: 'relative',
+            margin: 'auto'
+        });
+
+    }, [windowDimensions]);
+
     return (
-        <div className='gameSection' style={cssStyle}>
-            <GameBoard parentWidth={gameWidth} parentHeight={gameHeight} />
+        <div className='gameSection' style={gameBoardCssStyle}>
+            <GameBoard parentWidth={gameBoardDimensions.width} parentHeight={gameBoardDimensions.height} />
         </div>
     );
 }
