c02d1c1199b65d2323901a888846480ff907f3c5
Shubham-Vishwakarma <shubhamvishwakarma71@gmail.com>
2021-02-07 13:21:44 +0530

    Changing DotsAndBoxes to Functional Component

--- src/DotsAndBoxes.tsx
+++ src/DotsAndBoxes.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import Box from './Box';
 import Dot from './Dot';
@@ -19,91 +19,84 @@ type Props = {
     numberOfColumns: number
 }
 
-type State = {
-    boxes: Array<IBox>,
-    dots: Array<IDot>,
-    lines: Array<ILine>
-}
+function DotsAndBoxes(props: Props){
 
-class DotsAndBoxes extends React.Component<Props, State>{
+    const backgroundColorContext = useContext(BackgroundColorContext);
 
-    static contextType = BackgroundColorContext
+    const [boxes, setBoxes] = useState<Array<IBox>>([]);
+    const [dots, setDots] = useState<Array<IDot>>([]);
+    const [lines, setLines] = useState<Array<ILine>>([]);
 
-    constructor(props: Props){
-        super(props);
+    useEffect(
+        () => {
+            if(boxes.length === 0 && dots.length === 0 && lines.length === 0){
+                const nboxes = DotsAndBoxesHelper.createBoxes(props.parentWidth, props.parentHeight, props.numberOfRows, props.numberOfColumns);
+                const ndots = DotsAndBoxesHelper.createDots(props.parentWidth, props.parentHeight, props.numberOfRows, props.numberOfColumns);
+                const nlines = DotsAndBoxesHelper.createLines(props.parentWidth, props.parentHeight, props.numberOfRows, props.numberOfColumns);
 
-        this.state = {
-            boxes: [],
-            dots: [],
-            lines: []
+                setBoxes(nboxes);
+                setDots(ndots);
+                setLines(nlines);
+            }
+            else 
+            {
+                const nboxes = DotsAndBoxesHelper.updateBoxes(props.parentWidth, props.parentHeight, props.numberOfRows, props.numberOfColumns, boxes);
+                const ndots = DotsAndBoxesHelper.updateDots(props.parentWidth, props.parentHeight, props.numberOfRows, props.numberOfColumns, dots);
+                const nlines = DotsAndBoxesHelper.updateLines(props.parentWidth, props.parentHeight, props.numberOfRows, props.numberOfColumns, lines);
+           
+                setBoxes(nboxes);
+                setDots(ndots);
+                setLines(nlines);
+            }
+        }, [props, boxes, dots, lines]
+    );
+
+    function onMouseDown(line: ILine){
+        const nlines = DotsAndBoxesUpdater.updateLines(lines, line, backgroundColorContext.backgroundColor, true);
+        const ndots: Array<IDot> = DotsAndBoxesUpdater.updateDots(dots, line, 'rgba(0, 38, 97, 0.6)', true)
+        const nboxes: Array<IBox> = DotsAndBoxesUpdater.updateBoxes(boxes, line, backgroundColorContext.backgroundColor);
+
+        backgroundColorContext.changeBackgroundColor(backgroundColorContext.backgroundColor);
+        setBoxes(nboxes);
+        setDots(ndots);
+        setLines(nlines);
+    }
+
+    function onMouseEnter(line: ILine){
+        const nlines = DotsAndBoxesUpdater.updateLines(lines, line, 'rgba(0, 38, 97, 0.6)', false);
+        const ndots: Array<IDot> = DotsAndBoxesUpdater.updateDots(dots, line, 'rgba(0, 38, 97, 0.6)', false);
+        
+        setDots(ndots);
+        setLines(nlines);
+    }
+
+    function onMouseLeave(line: ILine){
+        const nlines = DotsAndBoxesUpdater.updateLines(lines, line, '#b5cef5', false);
+        const ndots: Array<IDot> = DotsAndBoxesUpdater.updateDots(dots, line, '#b5cef5', false);
+
+        setDots(ndots);
+        setLines(nlines);
+    }
+
+    return(
+        <div>
+        {
+            boxes.map((box) => (<Box key={box.id} box={box} />))
         }
-    }
+        {
+            lines.map((line) => (<Line  key={line.id} 
+                                                    line={line} 
+                                                    onMouseDown={onMouseDown}
+                                                    onMouseEnter={onMouseEnter}
+                                                    onMouseLeave={onMouseLeave}
+                                                />))
+        }
+        {
+            dots.map((dot) => (<Dot key={dot.id} dot={dot} />))
+        }
+        </div>
+    );
 
-    componentDidMount(){
-        const boxes = DotsAndBoxesHelper.createBoxes(this.props.parentWidth, this.props.parentHeight, this.props.numberOfRows, this.props.numberOfColumns);
-        const dots = DotsAndBoxesHelper.createDots(this.props.parentWidth, this.props.parentHeight, this.props.numberOfRows, this.props.numberOfColumns);
-        const lines = DotsAndBoxesHelper.createLines(this.props.parentWidth, this.props.parentHeight, this.props.numberOfRows, this.props.numberOfColumns);
-
-        this.setState({boxes: boxes, dots: dots, lines: lines});
-    }
-
-    componentDidUpdate(prevProps: Props){
-        if(prevProps.parentWidth !== this.props.parentWidth ||
-            prevProps.parentHeight !== this.props.parentHeight ||
-            prevProps.numberOfRows !== this.props.numberOfRows ||
-            prevProps.numberOfColumns !== this.props.numberOfColumns){
-                const boxes = DotsAndBoxesHelper.updateBoxes(this.props.parentWidth, this.props.parentHeight, this.props.numberOfRows, this.props.numberOfColumns, this.state.boxes);
-                const dots = DotsAndBoxesHelper.updateDots(this.props.parentWidth, this.props.parentHeight, this.props.numberOfRows, this.props.numberOfColumns, this.state.dots);
-                const lines = DotsAndBoxesHelper.updateLines(this.props.parentWidth, this.props.parentHeight, this.props.numberOfRows, this.props.numberOfColumns, this.state.lines);
-        
-                this.setState({boxes: boxes, dots: dots, lines: lines});
-            }
-    }
-    
-    private onMouseDown(line: ILine){
-        const lines = DotsAndBoxesUpdater.updateLines(this.state.lines, line, this.context.backgroundColor, true);
-        const dots: Array<IDot> = DotsAndBoxesUpdater.updateDots(this.state.dots, line, 'rgba(0, 38, 97, 0.6)', true)
-        const boxes: Array<IBox> = DotsAndBoxesUpdater.updateBoxes(this.state.boxes, line, this.context.backgroundColor);
-
-        this.context.changeBackgroundColor(this.context.backgroundColor);
-
-        this.setState({lines: lines, dots: dots, boxes: boxes})
-    }
-
-    private onMouseEnter(line: ILine){
-        const lines = DotsAndBoxesUpdater.updateLines(this.state.lines, line, 'rgba(0, 38, 97, 0.6)', false);
-        const dots: Array<IDot> = DotsAndBoxesUpdater.updateDots(this.state.dots, line, 'rgba(0, 38, 97, 0.6)', false);
-        
-        this.setState({lines: lines, dots: dots});
-    }
-
-    private onMouseLeave(line: ILine){
-        const lines = DotsAndBoxesUpdater.updateLines(this.state.lines, line, '#b5cef5', false);
-        const dots: Array<IDot> = DotsAndBoxesUpdater.updateDots(this.state.dots, line, '#b5cef5', false);
-
-        this.setState({lines: lines, dots: dots});
-    }
-
-    render(){
-        return(
-            <div>
-            {
-                this.state.boxes.map((box) => (<Box key={box.id} box={box} />))
-            }
-            {
-                this.state.lines.map((line) => (<Line  key={line.id} 
-                                                        line={line} 
-                                                        onMouseDown={this.onMouseDown.bind(this)}
-                                                        onMouseEnter={this.onMouseEnter.bind(this)}
-                                                        onMouseLeave={this.onMouseLeave.bind(this)}
-                                                    />))
-            }
-            {
-                this.state.dots.map((dot) => (<Dot key={dot.id} dot={dot} />))
-            }
-            </div>
-        );
-    }
 }
 
 export default DotsAndBoxes;
